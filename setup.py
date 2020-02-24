198639bd8cb304e06f6fd2f1903448e07415dcf0
Diarmuid Collins <diarmuid.m.collins@gmail.com>
2020-02-24 21:08:36 +0000

    Fixed path

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Newkiton temperature sensor",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/Newkiton",
+    url="https://github.com/diarmuid/Newkiton",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
